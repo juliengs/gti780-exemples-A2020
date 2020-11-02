cd7e9b7a14a178e3fa7c490ca2d71392ad29ef25
Julien Gascon-Samson <julien.gascon-samson@etsmtl.ca>
2020-11-02 17:20:37 -0500

    Exemples de code cours 2 pubsub

---
+++ cours2-pubsub/pubsub.js
@@ -0,0 +1,31 @@
+class PubSub {
+
+  constructor() {
+    // Pas d'arguments
+    // Initialisez les variables d'instance de classe ici avec le mot-clé this
+    // Exemple:
+    // this.myVar = 42;
+    // this.myArr = [];
+    // this.myDict = {};
+  }
+
+  publish(/* Quels sont les paramètres? */) {
+    // Implémentez votre code ici
+  }
+
+  subscribe(/* Quels sont les paramètres? */) {
+    // Implémentez votre code ici
+  }
+
+  unsubscribe(/* Quels sont les paramètres? */) {
+    // Implémentez votre code ici
+  }
+
+}
+
+// Cas de test
+let pubsub = new PubSub();
+
+// Implémentez quelques cas de tests:
+// Souscriptions et désouscriptions
+// Publications
